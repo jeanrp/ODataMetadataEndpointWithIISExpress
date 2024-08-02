8abd5e090ab0f9ed682335b74dee6d69d44dbb22
Jean Ribeiro Pinto <jeanribeirop@gmail.com>
2024-08-02 09:42:00 -0400

    initial files

---
+++ ODataMetadataEndpointWithIISExpress.WebApi/Entities.cs
@@ -0,0 +1,17 @@
+﻿namespace ODataMetadataEndpointWithIISExpress.WebApi
+{
+    public class Entity1
+    {
+        public int Id { get; set; }
+    }
+
+    public class Entity2
+    {
+        public int Id { get; set; } 
+
+        public Entity1 Entity1 { get; set; }
+
+        public int Entity1Id { get; set; }
+    }
+
+}
---
+++ ODataMetadataEndpointWithIISExpress.WebApi/Startup.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNet.OData.Builder;
+using Microsoft.AspNet.OData.Extensions;
+using Microsoft.AspNet.OData.Routing;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Internal;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OData;
+using Microsoft.OData.Edm;
+using ODataMetadataEndpointWithIISExpress.WebApi;
+using System;
+using System.Linq;
+
+namespace WebApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+         
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddOData();
+        }
+        
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+                app.UseDeveloperExceptionPage();
+
+            app.UseMvc(routeBuilder =>
+            {                
+                routeBuilder.EnableDependencyInjection();
+
+                routeBuilder.MapODataServiceRoute("odata", null, GetEdmModel());
+
+                routeBuilder.Select().Expand().Filter().MaxTop(null).Count().OrderBy(); 
+            });
+        }
+
+        private static IEdmModel GetEdmModel()
+        {
+            var builder = new ODataConventionModelBuilder();
+
+            builder.EntitySet<Entity1>("Entity1s");
+            builder.EntitySet<Entity2>("Entity2s");
+
+            return builder.GetEdmModel();
+        }
+    }
+}
+
